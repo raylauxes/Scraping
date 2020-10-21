975ecdba1a9499fb76600c092943fca48933e54f
raylauxes <raylauxeswu@yahoo.com.tw>
2020-10-21 13:24:51 +0900

    googleCrawler.py: Output as whole string

--- googleCrawler.py
+++ googleCrawler.py
@@ -1,45 +1,41 @@
 import time
-
-### from random import random
 import random
 from selenium import webdriver
 
 def top_10_sites(vocab_list, target_url='https://www.google.com/'):
 
     driver = webdriver.Chrome("C:\WORK\GitHub\Scraping\chromedriver.exe")
-    # OR
-    # driver = webdriver.Chrome() #Need to specify chromedriver.exe's location in PATH
+   #OR
+   #driver = webdriver.Chrome() #Need to specify chromedriver.exe's location in PATH
 
-    #Turn input words into a search string
-    search_string = '"' + f'" "'.join(vocab_list) + '"'
+    search_string = '"' + f'" "'.join(vocab_list) + '"' #Turn input words into a search string
+    wait = random.random() #Set wait time to avoid frequent access to search engine
+    wait_times = 1
+    driver.get(target_url) #Access the target url
+    time.sleep(wait * wait_times)
 
-    #Set wait time to avoid frequent access to search engine
-    wait = random.random()
-
-    #Access the target url
-    driver.get(target_url)
-
-    time.sleep(wait * 1)
-
-    #Enter serach string in search box
-    search_box = driver.find_element_by_name("q")
+    search_box = driver.find_element_by_name("q") #Enter serach string in search box
     search_box.send_keys(search_string)
+    time.sleep(wait * wait_times)
 
-    time.sleep(wait * 1)
+    search_box.submit() #Submit (equivalent to pressing "Enter")
+    time.sleep(wait * wait_times)
 
-    #Submit (equivalent to pressing "Enter")
-    search_box.submit()
-
-    time.sleep(wait * 1)
-
-    result = []
+    # result = []
+    # for element in driver.find_elements_by_class_name("aCOpRe"):
+    #     result.append(element.text)
+    #     print(element.text)
+    #     print("-"*100)
+    raw_sen = ""
     for element in driver.find_elements_by_class_name("aCOpRe"):
-        result.append(element.text)
-        print(element.text)
-        print("-"*100)
+        raw_sen += element.text
+        # print(element.text)
+        # print("-"*100)
 
 
-    #Quit Chrome
-    # driver.quit()
 
-    return result
+
+
+    # driver.quit() #Quit Chrome
+
+    return raw_sen
