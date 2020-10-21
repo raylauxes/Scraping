220a167c599ad31e2a0a884a769f0ac87549d3ae
raylauxes <raylauxeswu@yahoo.com.tw>
2020-10-21 13:29:06 +0900

    googleCrawler.py: Updated code

--- googleCrawler.py
+++ googleCrawler.py
@@ -1,45 +1,44 @@
 import time
-
-from random import random
+import random
 from selenium import webdriver
 
-def top_10_sites(vocab_list):
-    # driver = webdriver.Chrome() #Need to specify chromedriver.exe's location in PATH
-    # OR specify it as an argument like this:
-    driver = webdriver.Chrome("C:\WORK\GitHub\Scraping\chromedriver.exe")
+class gCrawler
+    def __init__(self):
 
-    just_a_sec = random()
+    def top_10_sites(self, vocab_list, target_url='https://www.google.com/'):
 
-    search_string = ""
-    for i in vocab_list:
-        search_string = search_string + i + '"　"'
+        driver = webdriver.Chrome("C:\WORK\GitHub\Scraping\chromedriver.exe") #Need to download chromdriver.exe
+       #OR
+       #driver = webdriver.Chrome() #Need to specify chromedriver.exe's location in PATH
 
-    driver.get('https://www.google.com/')
-    time.sleep(just_a_sec * 1)
-    search_box = driver.find_element_by_name("q")
-    search_box.send_keys('"' + search_string + '"')
-    time.sleep(just_a_sec * 1)
-    search_box.submit()
-    time.sleep(just_a_sec * 1)
+        search_string = '"' + f'" "'.join(vocab_list) + '"' #Turn input words into a search string
+        wait = random.random() #Set wait time to avoid frequent access to search engine
+        wait_times = 1
+        driver.get(target_url) #Access the target url
+        time.sleep(wait * wait_times)
 
-    result = []
-    # for h1 in driver.find_elements_by_tag_name("h1"):
-    #    print(h1.text)
-    for element in driver.find_elements_by_class_name("aCOpRe"):
-        result.append(element.text)
-        print(element.text)
-        print("-"*100)
+        search_box = driver.find_element_by_name("q") #Enter serach string in search box
+        search_box.send_keys(search_string)
+        time.sleep(wait * wait_times)
 
+        search_box.submit() #Submit (equivalent to pressing "Enter")
+        time.sleep(wait * wait_times)
 
-    # url_list = []
-    # XPATH = '//*[@id="rso"]/div/div/div[" + str(i + 1) + "]/div/div/div[1]/a'
-    # for a in driver.find_elements_by_xpath(XPATH):
-    #     url_list.append(a.get_attribute('href'))
-    #
-    # for i in url_list:
-    #     print(i)
+        # result = []
+        # for element in driver.find_elements_by_class_name("aCOpRe"):
+        #     result.append(element.text)
+        #     print(element.text)
+        #     print("-"*100)
+        raw_sen = ""
+        for element in driver.find_elements_by_class_name("aCOpRe"):
+            raw_sen += element.text
+            # print(element.text)
+            # print("-"*100)
 
 
-    driver.quit()
 
-    return result
+
+
+        # driver.quit() #Quit Chrome
+
+        return raw_sen
