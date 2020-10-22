4f00cab2294cc74c7a7b653aafb112d03f8fbed7
raylauxes <raylauxeswu@yahoo.com.tw>
2020-10-22 09:59:15 +0900

    Add: def output(self, relevant_only=False)

--- googleCrawler.py
+++ googleCrawler.py
@@ -1,22 +1,27 @@
-import time
+import pandas as pd
 import random
+import time
 from selenium import webdriver
 
 class gCrawler:
 
-    def __init__(self):
-        pass
+    # def __init__(self):
+    #     pass
+    def __init__(self, vocab_list, target_url='https://www.google.com/'):
+        self.vocab_list = vocab_list
+        self.target_url = target_url
 
-    def top_10_sites(self, vocab_list, target_url='https://www.google.com/'):
+    # def top_10_sites(self, vocab_list, target_url='https://www.google.com/'):
+    # def top_10_sites(self, target_url='https://www.google.com/'):
 
         driver = webdriver.Chrome("C:\WORK\GitHub\Scraping\chromedriver.exe") #Need to download chromdriver.exe
        #OR
        #driver = webdriver.Chrome() #Need to specify chromedriver.exe's location in PATH
 
-        search_string = '"' + f'" "'.join(vocab_list) + '"' #Turn input words into a search string
+        search_string = '"' + f'" "'.join(self.vocab_list) + '"' #Turn input words into a search string
         wait = random.random() #Set wait time to avoid frequent access to search engine
         wait_times = 1
-        driver.get(target_url) #Access the target url
+        driver.get(self.target_url) #Access the target url
         time.sleep(wait * wait_times)
 
         search_box = driver.find_element_by_name("q") #Enter serach string in search box
@@ -26,15 +31,38 @@ class gCrawler:
         search_box.submit() #Submit (equivalent to pressing "Enter")
         time.sleep(wait * wait_times)
 
-        raw_sen = ""
+        # self.raw_sen = ""
+        # for element in driver.find_elements_by_class_name("aCOpRe"):
+        #     self.raw_sen += element.text
+        self.raw_sen = []
         for element in driver.find_elements_by_class_name("aCOpRe"):
-            raw_sen += element.text
+            self.raw_sen.append(element.text)
 
 
+    def output(self, relevant_only=False):
 
+        if not relevant_only:
+            result = pd.DataFrame(self.raw_sen)
+
+        else:
+            raw_text = ""
+            for sen in self.raw_sen:
+                 raw_text += sen
+            result_in_one = "".join(raw_text)
+            processed_sen = [sentence + "。" for sentence in result_in_one.split("。")]
+
+            trimmed_sen = []
+            for sen in processed_sen:
+                if any([substr in sen for substr in self.vocab_list]):
+                    trimmed_sen.append(sen)
+
+            result = pd.DataFrame(trimmed_sen)
+
+
+        result.to_csv(f"search_results_{'_'.join(self.vocab_list)}.csv", encoding="utf-8-sig")
 
 
 
         # driver.quit() #Quit Chrome
 
-        return raw_sen
+        return result
